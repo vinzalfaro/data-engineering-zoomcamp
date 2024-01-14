e45f8ce379526c68364dfe654b23e0af2e5d8bc0
Vinz <120233304+entropy143@users.noreply.github.com>
2024-01-14 14:37:32 +0800

    Modify to upload csv instead of parquet files

--- 3_data_warehouse/web_to_gcs.py
+++ 3_data_warehouse/web_to_gcs.py
@@ -5,7 +5,7 @@ import pandas as pd
 from google.cloud import storage
 
 """
-Pre-reqs: 
+Pre-reqs:
 1. `pip install pandas pyarrow google-cloud-storage`
 2. Set GOOGLE_APPLICATION_CREDENTIALS to your project/service-account key
 3. Set GCP_GCS_BUCKET as your bucket or change default value of BUCKET
@@ -26,7 +26,7 @@ def upload_to_gcs(bucket, object_name, local_file):
     # storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
     # storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
 
-    client = storage.Client(project='massive-haiku-407306, ')
+    client = storage.Client(project='massive-haiku-407306')
     bucket = client.bucket(bucket)
     blob = bucket.blob(object_name)
     blob.upload_from_filename(local_file)
@@ -50,16 +50,18 @@ def web_to_gcs(year, service):
 
         # read it back into a parquet file
         df = pd.read_csv(file_name, compression='gzip')
-        file_name = file_name.replace('.csv.gz', '.parquet')
-        df.to_parquet(file_name, engine='pyarrow')
-        print(f"Parquet: {file_name}")
+        file_name = file_name.replace('.csv.gz', '.csv')
+        df.to_csv(file_name, index=False, compression='gzip')
+        print(f"CSV: {file_name}")
 
         # upload it to gcs 
         upload_to_gcs(BUCKET, f"{service}/{file_name}", file_name)
         print(f"GCS: {service}/{file_name}")
 
+        os.remove(file_name)
 
-# web_to_gcs('2019', 'green')
-# web_to_gcs('2020', 'green')
-web_to_gcs('2019', 'yellow')
-web_to_gcs('2020', 'yellow')
+
+web_to_gcs('2019', 'green')
+web_to_gcs('2020', 'green')
+# web_to_gcs('2019', 'yellow')
+# web_to_gcs('2020', 'yellow')
